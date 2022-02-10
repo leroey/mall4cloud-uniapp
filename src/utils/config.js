61d20274d2f3016ede37e8633353c0f427d4c756
gongruo.lgr <gongruo.lgr@alibaba-inc.com>
2022-02-10 19:58:18 +0800

    use ip

--- src/utils/config.js
+++ src/utils/config.js
@@ -8,15 +8,15 @@ var mpAppId = 'wx42bbe857570c5b40'
 
 // 本地运行环境 ->  地址修改为本机IP; 端口与 manifest.json 中 h5 -> devServer -> port 一致;
 // 运行后浏览器使用 "http://本机IP+端口" 访问
-var domain = 'http://sharerare.store:8000' // 统一接口域名，测试环境
+var domain = 'http://47.96.20.186:8000' // 统一接口域名，测试环境
 // var domain = 'http://sharerare.store:8000' // 统一接口域名，测试环境
-var wsDomain = 'ws://sharerare.store:8000' // 统一接口域名，测试环境
+var wsDomain = 'ws://47.96.20.186:8000' // 统一接口域名，测试环境
 // var wsDomain = 'ws://sharerare.store:8000' // 统一接口域名，测试环境
 
 var picDomain = 'http://img-test.gz-yami.com/' // 图片域名
 
 // 访问文件存储资源的url 对应阿里云的Bucket域名
-var resourcesUrl = 'http://sharerare.store:9000/mall4cloud'
+var resourcesUrl = 'http://47.96.20.186:9000/mall4cloud'
 // var resourcesUrl = 'http://sharerare.store:9000/mall4cloud'
 // var resourcesUrl = 'http://192.168.1.107:9000/mall4cloud'
 
